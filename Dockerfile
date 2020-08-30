17ec08a4aa4aba44b9b4767311706611e36431a7
kynefuk <atsuminamihira@gmail.com>
2020-08-30 15:10:22 +0900

    fix

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.8-slim-buster
-WORKDIR /app
-COPY requirements.txt /app/requirements.txt
-RUN pip install -r requirements.txt
-COPY . /app
-EXPOSE 8000
--- core/custom_permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-
-class ProfilePermission(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        """自分のProfileのみ更新・削除できる"""
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.userPro.id == request.user.id
